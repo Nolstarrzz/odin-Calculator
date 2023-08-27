980a6d6bf214a56b9aa9d4ae2be1430d7ca3ddb8
Nolstarrzz <125843678+Nolstarrzz@users.noreply.github.com>
2023-08-27 16:25:26 -0700

    calculator has bugs but works, all ops work
    need to add decimal values and what user is clicking on,
    need to add error if user trys 0 divide and what happens if user clicks equals too early

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 const table = document.querySelector('.calculations-table');
 const display = document.querySelector('.display')
-let number1 = 1;
-let number2 = 6;
-let operator = "*";
+let number1 =  0;
+let number2 = 0;
 let answer = 0;
+let operator = '';
  
 table.addEventListener('click', (event) => {
     if (event.target.tagName === 'BUTTON') {
-        const buttonText = event.target.textContent;
+        let buttonText = event.target.textContent;
         if( buttonText === '1' || buttonText === '2'|| 
             buttonText === '3' || buttonText === '4' ||
             buttonText === '5' || buttonText === '6' ||
             buttonText === '7' || buttonText === '8' ||
-            buttonText === '9')
+            buttonText === '9' || buttonText === '0')
         {
-            display.textContent = display.textContent + buttonText;
+            if(number1 === 0 || operator === '' && answer != 0)
+            {
+                display.textContent = display.textContent + buttonText;
+                number1 = display.textContent;
+                console.log(number1);
+            }
+            else
+            {
+                display.textContent = display.textContent + buttonText;
+                number2 = display.textContent;
+                console.log(number2)
+            }
         }
         else if(buttonText === 'CLEAR')
         {
             display.textContent = '';
+            number1 = 0;
+            number2 = 0;
+            operator = '';
         }
         else if(buttonText === 'DELETE')
         {
@@ -26,6 +40,21 @@ table.addEventListener('click', (event) => {
             if (currentText.length > 0) {
                 display.textContent = currentText.slice(0, -1);
             }
+        }
+        else if(buttonText === '+' || buttonText === '-' || 
+                buttonText === '×' || buttonText === '÷')
+        {
+            operator = buttonText;
+            display.textContent = '';
+        }
+        else if(buttonText === '=')
+        {
+            display.textContent = '';
+            number2 = parseInt(number2)
+            number1 = parseInt(number1)
+            operate(number1, operator, number2)
+            operator = '';
+            equals(answer)
         }
     }
 });
@@ -36,9 +65,9 @@ function operate(number1, operator, number2)
         add(number1, number2)
     else if(operator === '-')
         subtract(number1, number2)
-    else if(operator === '*')
+    else if(operator === '×')
         multiply(number1, number2)
-    else if(operator === '/')
+    else if(operator === '÷')
         divide(number1, number2)
     return answer;
 }
@@ -62,4 +91,11 @@ function multiply(number1, number2)
 function divide(number1, number2)
 {
     return answer = number1 / number2;
+}
+
+function equals(answer)
+{
+    display.textContent = '';
+    display.textContent = answer;
+    number1 = answer;
 }
