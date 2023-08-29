7c57cc91478e906945dcc4063b47d63f9795114c
Nolstarrzz <125843678+Nolstarrzz@users.noreply.github.com>
2023-08-28 17:01:31 -0700

    delete now works, with some bugs, project done
    need to fix bugs with delete like NaN showing up after deleting the last number and pressing the operations

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ let number1 =  0;
 let number2 = 0;
 let answer = 0;
 let operator = '';
+let decimal = false;
+let error = false;
+
  
 table.addEventListener('click', (event) => {
+    if(error === true)
+        display.textContent = '';
+        error = false;
     if (event.target.tagName === 'BUTTON') {
         let buttonText = event.target.textContent;
         if( buttonText === '1' || buttonText === '2'|| 
@@ -14,17 +20,28 @@ table.addEventListener('click', (event) => {
             buttonText === '7' || buttonText === '8' ||
             buttonText === '9' || buttonText === '0')
         {
-            if(number1 === 0 || operator === '' && answer != 0)
+            if(number1 === 0 || operator === '')
             {
                 display.textContent = display.textContent + buttonText;
                 number1 = display.textContent;
-                console.log(number1);
+                /*if(decimal)
+                {
+                    number1 = parseFloat(number1)
+                    console.log(number1)
+                }*/
+                    number1 = parseInt(number1)
+
             }
             else
             {
                 display.textContent = display.textContent + buttonText;
                 number2 = display.textContent;
-                console.log(number2)
+                /*if(decimal)
+                {
+                    number2 = parseFloat(number2)
+                    console.log(number2)
+                }*/
+                number2 = parseInt(number2)
             }
         }
         else if(buttonText === 'CLEAR')
@@ -33,12 +50,29 @@ table.addEventListener('click', (event) => {
             number1 = 0;
             number2 = 0;
             operator = '';
+            decimal = false;
         }
         else if(buttonText === 'DELETE')
         {
-            const currentText = display.textContent;
-            if (currentText.length > 0) {
+            let ifNum1 = false;
+            let nums = display.textContent;
+            nums = parseInt(nums)
+            if(nums === number1)
+                ifNum1 = true;
+            let currentText = display.textContent;
+            if (currentText.length > 0) 
+            {
                 display.textContent = currentText.slice(0, -1);
+                nums = display.textContent;
+            }
+            if(ifNum1 === true)
+            {
+                number1 = nums;
+                display.textContent = number1;
+            }
+            else{
+                number2 = nums;
+                display.textContent = number2;
             }
         }
         else if(buttonText === '+' || buttonText === '-' || 
@@ -50,14 +84,35 @@ table.addEventListener('click', (event) => {
         else if(buttonText === '=')
         {
             display.textContent = '';
-            number2 = parseInt(number2)
             number1 = parseInt(number1)
+            number2 = parseInt(number2)
             operate(number1, operator, number2)
             operator = '';
             equals(answer)
         }
+        else if(buttonText === '.')
+        {
+            /*decimal = true;
+            if(!display.textContent.includes('.'))
+                display.textContent += '.';
+                number1 += '.'
+                number2 += '.'
+                console.log(number1)
+                console.log(number2)*/
+        }
+
     }
 });
+
+/*function deleter()
+{
+    let currentText = display.textContent;
+    if (currentText.length > 0) {
+        display.textContent = currentText.slice(0, -1);
+        nums = display.textContent;
+        console.log(nums)
+    }
+}*/
 
 function operate(number1, operator, number2)
 {
@@ -90,7 +145,20 @@ function multiply(number1, number2)
 
 function divide(number1, number2)
 {
-    return answer = number1 / number2;
+    if(number1 === 0 || number2 === 0)
+    {
+        alert("YOU DING DONG, YOU TRIED DIVIDING BY 0")
+        error = true;
+        return;
+    }
+    else if(Number.isInteger(number1 / number2))
+        answer = number1 / number2
+    else
+    {
+        answer = number1 / number2
+        answer = Math.round(answer * 100) / 1000;
+    }
+    return answer;
 }
 
 function equals(answer)
